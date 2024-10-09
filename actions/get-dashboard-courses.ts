fa3d120efb9cffa802b754a4c16fe91fcaab76b3
Frankz Alarcon <leninalarcon7@gmail.com>
2024-10-09 09:31:47 -0500

    students dashboard and analytics

---
+++ actions/get-dashboard-courses.ts
@@ -0,0 +1,55 @@
+import { db } from "@/lib/db"
+import { Category, Chapter, Course } from "@prisma/client"
+import { getProgress } from "./get-progress"
+
+type CourseWithProgressWithCategory = Course & {
+  category: Category
+  chapters: Chapter[]
+  progress: number | null
+}
+
+type DashboardCourses= {
+  completedCourses: CourseWithProgressWithCategory[]
+  coursesInProgress: CourseWithProgressWithCategory[]
+}
+
+export const getDashboardCourses = async (userId: string): Promise<DashboardCourses> => {
+  try {
+    const purchasedCourses = await db.purchase.findMany({
+      where: {
+        userId
+      },
+      select: {
+        course: {
+          include: {
+            category: true,
+            chapters: {
+              where: {
+                isPublished: true
+              }
+            }
+          }
+        }
+      }
+    })
+    const courses = purchasedCourses.map(purchasedCourse => purchasedCourse.course) as CourseWithProgressWithCategory[]
+
+    for (const course of courses) {
+      const progress = await getProgress(userId, course.id)
+      course['progress'] = progress    
+    }
+    const completedCourses = courses.filter(course => course.progress === 100)
+    const coursesInProgress = courses.filter(course => (course.progress ?? 0) < 100)
+
+    return {
+      completedCourses,
+      coursesInProgress
+    }
+  } catch (error) {
+    console.log("GET DASHBOARD COURSES", error)
+    return {
+      completedCourses: [],
+      coursesInProgress: []
+    }
+  }
+}
